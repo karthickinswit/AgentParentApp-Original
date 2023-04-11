1f0e1b0434ba9b9cc143edd941862e46d6e65b5e
shanjayappiyo <124661127+shanjayappiyo@users.noreply.github.com>
2023-04-11 16:17:34 +0530

    FCM Implementation by Shanjay

--- App.js
+++ App.js
@@ -15,10 +15,12 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {FAB} from 'react-native-elements';
 import {LogBox} from 'react-native';
 import JustInTime from 'chatagentsdk/src/screens/JustInScreen';
-import {notificationListener, requestUserPermission} from './pushNotifications';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const ThemeContext = React.createContext({});
 export default function ChatParent() {
+  const navigation = useNavigation();r
   const Stack = createStackNavigator();
   LogBox.ignoreAllLogs();
 
@@ -26,6 +28,66 @@ export default function ChatParent() {
     requestUserPermission();
     notificationListener();
   }, []);
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+      fcmToken();
+    }
+  };
+
+  const fcmToken = async () => {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    console.log(fcmToken, 'the Old Token');
+    if (!fcmToken) {
+      console.log('No token found in storage');
+      try {
+        fcmToken = await messaging().getToken();
+        if (fcmToken) {
+          console.log(fcmToken, 'the new one');
+          await AsyncStorage.setItem('fcmToken', fcmToken); // fix the typo
+        }
+      } catch (error) {
+        console.log('Error in Fcm Token', error);
+      }
+    }
+  };
+
+  const notificationListener = async () => {
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.data,
+      );
+      navigation.navigate(remoteMessage.notification.android.clickAction);
+    });
+
+    messaging().onMessage(async remoteMessage => {
+      console.log('Message received in foreground', remoteMessage);
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message received in Background', remoteMessage);
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.data,
+          );
+          // setInitialRoute(remoteMessage.data.type);
+        }
+        // setLoading(false);
+      });
+  };
 
   return (
     <NavigationContainer>
